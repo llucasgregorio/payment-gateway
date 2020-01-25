37aa7742db534f1c6795464bcacdde31e9584d2c
eichild <luucasgregorio@gmail.com>
2020-01-25 01:03:38 -0300

    Adicionado payment controller, model

--- backend/models/transaction.js
+++ backend/models/transaction.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
 const TransactionSchema = new mongoose.Schema({
-	user :{
+    
+    user :{
         type :mongoose.Schema.Types.ObjectId,
         ref:'User'
     },
     amount :{
-        type : Number,
+        type : mongoose.Schema.Types.String,
         required : true
+    },
+    payment:{
+        type: mongoose.Schema.Types.ObjectID,
+        required: true
     }
 
 });			
